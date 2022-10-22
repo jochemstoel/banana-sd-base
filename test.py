5c40b87583dae98d39a4d366ff0c463066317b76
Jochem Stoel <jochemstoel@gmail.com>
2022-10-22 23:07:04 +0200

    bnmnbmbnmmnbmbnnmbbnm

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ TESTS = "tests"
 FIXTURES = TESTS + os.sep + "fixtures"
 OUTPUT = TESTS + os.sep + "output"
 
-
+# 
 def b64encode_file(filename: str):
     with open(os.path.join(FIXTURES, filename), "rb") as file:
         return base64.b64encode(file.read())
